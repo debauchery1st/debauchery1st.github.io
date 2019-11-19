f556f9d3cdedea186dfe075d1ef99576fa6d08eb
debauchery1st <trevor.j.martin@gmail.com>
2019-11-19 16:35:41 -0500

    test page about

--- plants/js/index.js
+++ plants/js/index.js
@@ -2,5 +2,5 @@ var plantNum = 1;
 var plantShow = setInterval(cycleBG, 7000);
 function cycleBG() {
     document.getElementsByClassName("hero").heroImg.style.backgroundImage=`url('img/bg${plantNum}.jpg')`;
-    plantNum = (plantNum === 9) ? 1:plantNum+1;
+    plantNum = (plantNum === 4) ? 1:plantNum+1;
 }
