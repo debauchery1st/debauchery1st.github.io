10b927779f5030d280a23c4cca7ecedae09799c1
debauchery1st <trevor.j.martin@gmail.com>
2019-11-19 20:29:48 -0500

    test page about

---
+++ plants/update.sh
@@ -0,0 +1,10 @@
+
+cp ../../PlantParenthood/Marketing-page/*.html .
+cp -r ../../PlantParenthood/Marketing-page/css .
+cp -r ../../PlantParenthood/Marketing-page/js .
+cp -r ../../PlantParenthood/Marketing-page/img .
+
+
+
+
+
