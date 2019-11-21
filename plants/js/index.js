7835d122014c270396fa733be49173b49e2d283d
debauchery1st <trevor.j.martin@gmail.com>
2019-11-21 14:52:24 -0500

    testing page

--- plants/js/index.js
+++ plants/js/index.js
@@ -1,6 +1,12 @@
 var plantNum = 1;
 var plantShow = setInterval(cycleBG, 7000);
+const nextImage = (pre, n, ext) => `url('${pre}${n}${ext}')`;
+const imgFolder = "img/bg";
+
 function cycleBG() {
-    document.getElementsByClassName("hero").heroImg.style.backgroundImage=`url('img/bg${plantNum}.jpg')`;
+    document.getElementsByClassName("hero")
+    .heroImg
+    .style
+    .backgroundImage = nextImage(imgFolder, plantNum, ".jpg");
     plantNum = (plantNum === 4) ? 1:plantNum+1;
 }
