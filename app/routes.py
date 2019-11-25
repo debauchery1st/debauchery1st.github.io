fc0b7f075ff99ab78f64c205389587483e1688d7
debauchery1st <trevor.j.martin@gmail.com>
2019-11-25 16:38:51 -0500

    added static urls

--- app/routes.py
+++ app/routes.py
@@ -1,47 +1,29 @@
 from flask import render_template
 from app import app
 
-with open("/js/index.js") as f:
-    javascript = f.read()
-
-f.close()
-
-with open("/css/index.css") as f:
-    style = f.read()
-
-f.close()
-
-@app.route("/css")
-def css():
-    return style
-
-@app.route("/script")
-def javascript():
-    return javascript
-    
 @app.route("/")
 @app.route("/index")
 def index():
     user = {"username":"trevor"}
     links = [
         {'url': "https://www.python.org/", 'text':"[Python]"},
-        {'url': "learn/Learn Python The Hard Way, 3rd Edition .pdf", 'text': ". Learn Python The Hard Way"},
+        {'url': "static/learn/Learn Python The Hard Way, 3rd Edition .pdf", 'text': ". Learn Python The Hard Way"},
         {'url': "https://realpython.com/list-comprehension-python/", 'text': ". Python - (list comprehension)"},
         {'url': "https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world", 'text': ". Python - (flask mega tutorial)"}, 
         {'url': "https://kivy.org/", 'text': ". Python - (kivy GUI for RAD)"},
-        {'url': "learn/PythonNotesForProfessionals.pdf", 'text': ". Python - (notes)"},
+        {'url': "static/learn/PythonNotesForProfessionals.pdf", 'text': ". Python - (notes)"},
         {'url': "", 'text': "<br />"},
         {'url': "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript", 'text': "[Javascript]"},
         {'url': "https://developer.mozilla.org/en-US/docs/Web/JavaScript", 'text': ". Javascript - (MDN)"},
-        {'url': "learn/JavaScriptNotesForProfessionals.pdf", 'text': ". Javascript - (notes)"},
-        {'url': "learn/NodeJSNotesForProfessionals.pdf", 'text': ". NodeJS - (notes)"},
+        {'url': "static/learn/JavaScriptNotesForProfessionals.pdf", 'text': ". Javascript - (notes)"},
+        {'url': "static/learn/NodeJSNotesForProfessionals.pdf", 'text': ". NodeJS - (notes)"},
         {'url': "", 'text': "<br />"},
         {'url': "https://whatwg.org/", 'text': "[HTML]"},
         {'url': "https://developer.mozilla.org/en-US/docs/Web/HTML",'text': ". HTML - (MDN)"},
-        {'url': "learn/HTML5NotesForProfessionals.pdf", 'text': ". HTML5 - (notes)"},
+        {'url': "static/learn/HTML5NotesForProfessionals.pdf", 'text': ". HTML5 - (notes)"},
         {'url': "", 'text': "<br />"},
         {'url': "https://www.w3.org/Style/CSS/", 'text': "[CSS]"},
-        {'url': "https://developer.mozilla.org/en-US/docs/Web/CSS", 'url': ". CSS - (MDN)"},
+        {'url': "https://developer.mozilla.org/en-US/docs/Web/CSS", 'text': ". CSS - (MDN)"},
         {'url': "https://css-tricks.com/guides/",'text': ". CSS guides - (css-tricks)"},
         {'url': "", 'text': "<br />"},
         {'url': "http://www-formal.stanford.edu/jmc/recursive.pdf", 'text': "[LISP]"},
---
+++ app/static/link-static.sh
@@ -0,0 +1,5 @@
+#!/bin/env sh
+ln -s ../../css css
+ln -s ../../images images
+ln -s ../../js js
+ln -s ../../learn learn
