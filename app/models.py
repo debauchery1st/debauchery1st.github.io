c20bdf1b1bb5446ac688dd96b639b770aac52315
debauchery1st <trevor.j.martin@gmail.com>
2019-11-27 00:12:24 -0500

    email support

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from time import time
+import jwt
 from datetime import datetime
 from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -59,6 +61,20 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+    
     @classmethod 
     def default_avatar(*args, **kwargs):
         digest = md5(app.config['ANONYMOUS_SEED'].encode('utf-8')).hexdigest()
