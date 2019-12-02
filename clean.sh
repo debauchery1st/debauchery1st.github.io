c2fe8da580eeb251e2924e34918e151e5ffaf6b5
debauchery1st <trevor.j.martin@gmail.com>
2019-12-01 23:22:16 -0500

    removed redunancy

--- clean.sh
+++ clean.sh
@@ -1,11 +1,7 @@
 echo " -- removing Python cache --"
-rm -rf app/__pycache__
-rm -rf migrations/versions/__pycache__
-rm -rf migrations/__pycache__
+rm -rf */__pycache__
+rm -rf */*/__pycache__
 rm -rf __pycache__
 
 echo " -- removing Python virtual environment --"
 rm -rf venv
-echo " -- removing Python-C leftovers --"
-rm *.pyc
-rm */*.pyc
